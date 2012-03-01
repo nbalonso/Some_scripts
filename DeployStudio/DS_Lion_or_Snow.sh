6b08d9be180bb0ddf94f77dd86c0e965c8755557
nbalonso <nbalonso@gmail.com>
2012-03-01 13:50:49 +0300

    corrected processor check and exit

--- DeployStudio/DS_Lion_or_Snow.sh
+++ DeployStudio/DS_Lion_or_Snow.sh
@@ -4,22 +4,23 @@ MEMORY=`system_profiler SPHardwareDataType | grep "Memory" | awk '{ print $2; }'
 PROCESSOR=`system_profiler SPHardwareDataType | grep "Processor Name"`
 
 if [ $MEMORY -ge 2 ]; then
-        #echo "we have enough memory"
-        if [[ "$PROCESSOR" == *i7* ]]; then 
+    #echo "we have enough memory"
+    if [[ "$PROCESSOR" == *i7* ]]; then 
 		echo "RuntimeSelectWorkflow: Lion for this guy!"
+		exit 0
 	fi
 	if [[ "$PROCESSOR" == *i3* ]]; then 
 		echo "RuntimeSelectWorkflow: Lion for this guy!"
+		exit 0
 	fi
-	.
-	.
+	#
+	#
 	#more cpu checks here
-	.
-	.
-	
+	#
+	#
+	echo "RuntimeSelectWorkflow: Snow Leopard for this guy!"
+	exit 0	
 else
 	#echo "we do not have enough memory"
 	echo "RuntimeSelectWorkflow: Snow Leopard for this guy!"
 fi
-
-exit 0
